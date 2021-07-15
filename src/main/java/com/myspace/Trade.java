550a5dbc2b146ff6be7e19cddf2db25a7c45ef4a
taniamodi <>
2021-07-14 20:27:48 -0400

    {/src/main/java/com/myspace/Trade.java}

--- src/main/java/com/myspace/Trade.java
+++ src/main/java/com/myspace/Trade.java
@@ -11,6 +11,10 @@ public class Trade implements java.io.Serializable {
 	private java.lang.String buySellCode;
 	private java.lang.String buySideAccountIdentifier;
 
+	private java.lang.Integer id;
+
+	private java.lang.String name;
+
 	public Trade() {
 	}
 
@@ -31,10 +35,29 @@ public class Trade implements java.io.Serializable {
 		this.buySideAccountIdentifier = buySideAccountIdentifier;
 	}
 
+	public java.lang.Integer getId() {
+		return this.id;
+	}
+
+	public void setId(java.lang.Integer id) {
+		this.id = id;
+	}
+
+	public java.lang.String getName() {
+		return this.name;
+	}
+
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
 	public Trade(java.lang.String buySellCode,
-			java.lang.String buySideAccountIdentifier) {
+			java.lang.String buySideAccountIdentifier, java.lang.Integer id,
+			java.lang.String name) {
 		this.buySellCode = buySellCode;
 		this.buySideAccountIdentifier = buySideAccountIdentifier;
+		this.id = id;
+		this.name = name;
 	}
 
 }
