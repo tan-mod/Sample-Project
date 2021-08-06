66d0dccb13dce222cec96b2d7a88baaa8574925a
taniamodi <>
2021-08-06 12:45:01 -0400

    {/src/main/java/com/myspace/Trade.java}

--- src/main/java/com/myspace/Trade.java
+++ src/main/java/com/myspace/Trade.java
@@ -13,6 +13,9 @@ public class Trade implements java.io.Serializable {
 
 	private java.lang.Integer id;
 
+	@org.kie.api.definition.type.Label(value = "name")
+	private java.lang.String name;
+
 	public Trade() {
 	}
 
@@ -41,11 +44,21 @@ public class Trade implements java.io.Serializable {
 		this.id = id;
 	}
 
+	public java.lang.String getName() {
+		return this.name;
+	}
+
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
 	public Trade(java.lang.String buySellCode,
-			java.lang.String buySideAccountIdentifier, java.lang.Integer id) {
+			java.lang.String buySideAccountIdentifier, java.lang.Integer id,
+			java.lang.String name) {
 		this.buySellCode = buySellCode;
 		this.buySideAccountIdentifier = buySideAccountIdentifier;
 		this.id = id;
+		this.name = name;
 	}
 
 }
